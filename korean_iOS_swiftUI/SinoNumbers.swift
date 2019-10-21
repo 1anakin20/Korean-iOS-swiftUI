24347d54074450f65120a963964d457d92fcaefa
Tomas <tomasdaniel09@hotmail.com>
2019-10-20 21:50:06 -0400

    Added options view

--- korean_iOS_swiftUI/SinoNumbers.swift
+++ korean_iOS_swiftUI/SinoNumbers.swift
@@ -9,15 +9,50 @@
 import SwiftUI
 
 struct SinoNumbers: View {
+	@State private var showOptions: Bool = false
 	var body: some View {
 		VStack(spacing: 30) {
 			longNavigationLink(backgroundColor: .purple, text: "Korean to English")
 		}
+		.navigationBarItems(trailing:
+			Button("Options") {
+				// Present view options
+				self.showOptions = true
+			}.sheet(isPresented: self.$showOptions) {
+				options()
+			}
+		)
+	}
+}
+
+struct options: View {
+	@State var max: String = "100"
+	@State var min: String = "1"
+	var body: some View {
+		VStack(spacing: 50) {
+			HStack {
+				Text("Max")
+				Spacer()
+				TextField("Max", text: $max)
+					.frame(width: 55)
+			}
+			HStack {
+				Text("Min")
+				Spacer()
+				TextField("Min", text: $min)
+					.frame(width: 55)
+				
+			}
+		}
+		.multilineTextAlignment(.center)
+		.keyboardType(.numberPad)
+		.padding()
+		.offset(y: -170)
 	}
 }
 
 struct SinoNumbers_Previews: PreviewProvider {
 	static var previews: some View {
-		SinoNumbers()
+		options()
 	}
 }
