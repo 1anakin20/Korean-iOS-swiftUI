433acba8df591bb1314c827451535271a2562e7d
Tomas <tomasdaniel09@hotmail.com>
2020-01-09 13:23:56 -0500

    Added list view containing the Korean words and their English meaning

--- korean_iOS_swiftUI/Views/Dictionary/DictionaryView.swift
+++ korean_iOS_swiftUI/Views/Dictionary/DictionaryView.swift
@@ -8,82 +8,35 @@
 
 import SwiftUI
 
-class wordsArrayClass {
-	var wordsArray: [[String]] = [[""], [""]]
-	
-	func getKoreanWord() {
-		let parseJsonWordsArray = parseJson()
-		var tempArray: [[String]] = [[], []]
-		for item in 0..<parseJsonWordsArray.count {
-			tempArray[0].append(parseJsonWordsArray[item].fields[0])
-			tempArray[1].append(parseJsonWordsArray[item].fields[1])
-		}
-		wordsArray[0] = tempArray[0]
-		wordsArray[1] = tempArray[1]
-	}
-	
-}
-
 struct DictionaryView: View {
-	@State var wordsArrayObj = wordsArrayClass()
-	@State var wordsArray: [[String]] = [["", ""], ["", ""]]
-	
-	
-	func fillWordsArray() {
-		wordsArrayObj.getKoreanWord()
-		wordsArray = wordsArrayObj.wordsArray
-	}
-	
+	let wordsArray: [koreanWordsJson] = parseJson()
 	
 	var body: some View {
-		HStack {
-			ForEach(koreanWordsJson, id: koreanWordsJson.id) { item in
-				DictionaryRow(koreanWord: self.wordsArray[0][item], englishWord: self.wordsArray[1][item])
-			}
-		}.onAppear {
-			self.fillWordsArray()
+		List(wordsArray) { dictionaryRow in
+			DictionaryRow(dictionaryRow: dictionaryRow)
 		}
 	}
 }
 
 struct DictionaryRow: View {
-	var koreanWord: String
-	var englishWord: String
+	var dictionaryRow: koreanWordsJson
+	
 	var body: some View {
 		VStack {
 			HStack {
-				Text(koreanWord)
+				Text(dictionaryRow.fields[0])
 				Spacer()
 			}
 			HStack {
-				Text(englishWord)
+				Text(dictionaryRow.fields[1])
 				Spacer()
 			}
 		}.padding()
 	}
 }
 
-
-struct DictionaryDetail: View {
-	var koreanWord: String
-	var englishWord: String
-	
-	var body: some View {
-		HStack {
-			VStack {
-				Text(koreanWord)
-					.font(.largeTitle)
-				Text(englishWord)
-					.font(.title)
-				Spacer()
-			}.padding()
-			Spacer()
-		}
-	}
-}
-
 struct DictionaryView_Previews: PreviewProvider {
 	static var previews: some View {
-		DictionaryRow(koreanWord: "Korean word", englishWord: "english word")
+		DictionaryView()
 	}
 }
